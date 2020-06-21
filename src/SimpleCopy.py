d9ea921d86face9a1db963108683e97a0741050b
H Son <sonbyj01@gmail.com>
2020-06-21 11:38:21 -0400

    debugged and finally works:D

--- src/SimpleCopy.py
+++ src/SimpleCopy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from shutil import copy2
 from pathlib import Path
+import sys
 from .SourceFiles import SourceFiles
 
 
@@ -8,38 +9,28 @@ class SimpleCopy:
     def __init__(self, source):
         assert isinstance(source, SourceFiles), 'Not a SourceFiles object.'
         self.source_object = source
-
-    def _make_directory(self, target):
-        self.target_folder = Path(target)
-        if not self.target_folder.exists():
-            self.target_folder.mkdir(parents=True)
+        self.source_parent = self.source_object.source_path.parents[0]
 
     def simple_copy2(self, target):
-        self._make_directory(target)
-        # self.source_object.to_text_file(target_folder)
+        target_folder = Path(target)
 
-        for folder_path in self.source_object.records_folder:
-            print('4')
-            source_drive = folder_path.anchor
-            source_folder = Path(folder_path)
-            source_relative = source_folder.relative_to(source_drive)
+        if not target_folder.exists():
+            print('Doesn\'t exist') ######################################
+            target_folder.mkdir(parents=True)
 
-            new_path = self.target_folder.joinpath(source_relative)
-            print(new_path)
+        for source_folder_path in self.source_object.records_folder:
+            source_folder_path = Path(source_folder_path)
+            source_folder_relative_path = source_folder_path.relative_to(self.source_parent)
 
-            if not new_path.exists():
-                new_path.mkdir(parents=True)
+            target_absolute = target_folder.joinpath(source_folder_relative_path)
 
-        # for file_path in self.source_object.records_file:
-        #     source_drive = file_path.anchor
-        #     source_folder = Path(file_path)
-        #     source_relative = source_folder.relative_to(source_drive)
-        #
-        #     new_path = self.target_folder.joinpath(source_relative)
-        #
-        #     print('5')
-        #     copy2(file_path, new_path)
-        #     print('6')
+            if not target_absolute.exists():
+                target_absolute.mkdir(parents=True)
 
-    def something(self):
-        pass
+        for source_file_path in self.source_object.records_file:
+            source_file_path = Path(source_file_path)
+            source_file_relative_path = source_file_path.relative_to(self.source_parent)
+
+            target_absolute = target_folder.joinpath(source_file_relative_path)
+
+            copy2(source_file_path, target_absolute)
